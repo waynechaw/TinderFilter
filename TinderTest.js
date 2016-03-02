0f33a04681f9c9e27bd42c9cb838c68703860c70
jshaw22 <jshaw22@gmail.com>
2016-03-01 17:33:20 -0800

    Fixed async between tinder API and facial recognition. Need to return facial recognition objects

--- TinderTest.js
+++ TinderTest.js
@@ -6,16 +6,8 @@ var FB_USER_TOKEN = 'CAAGm0PX4ZCpsBAO8njUhAKIKuq30ZANBqTa3VEjdrgojm9C8c4VcPzkPkt
 var FB_USER_ID = '2539134351764';
 
 
-var URL = "http://i.imgur.com/REdTb8a.jpg";
-//var URL;
-
-var options = {
-    url: 'https://faceplusplus-faceplusplus.p.mashape.com/detection/detect?url='+URL,
-    headers: {
-      'X-Mashape-Key': 'IJ3moiqJsymshUNl41Sq09a38nMjp1OIrd9jsnJw6JjvebyGo5',
-      Accept: 'application/json'
-    }
-}
+//var URL = "http://i.imgur.com/REdTb8a.jpg";
+var URL;
 
 var requestCallback = function(error, response, body){
   console.log("Request Callback Invoked")
@@ -26,8 +18,16 @@ var requestCallback = function(error, response, body){
   }
 }
 
-var runFacialRecognition = function(params){
-  request(params, requestCallback);
+var runFacialRecognition = function(URL){
+  var options = {
+    url: 'https://faceplusplus-faceplusplus.p.mashape.com/detection/detect?url='+URL,
+    headers: {
+      'X-Mashape-Key': 'IJ3moiqJsymshUNl41Sq09a38nMjp1OIrd9jsnJw6JjvebyGo5',
+      Accept: 'application/json'
+    }
+  }
+
+  request(options, requestCallback);
 
 }
 
@@ -40,20 +40,9 @@ client.authorize(
       var pictureCount = 0;
       var pictureObj = {};
       data.results[0].photos.forEach(function(photo){
-        console.log(photo.url) // Print out all initial photos to compare and test
-
+        URL = photo.url;
+        console.log(URL)
+        runFacialRecognition(URL);
       });
     });
   });
-
-// var faceAnalysis = {
-//       gender: null,
-//       race: null,
-//     }
-//     faceAnalysis = {
-//       race: attributes.gender;
-//       gender: attributes.gender;
-//     };
-//     if(faceAnalysis.gender.value === 'Female' && faceAnalysis.gender.confidence > 50){
-
-//     }
