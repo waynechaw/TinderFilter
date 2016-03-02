c07cfbf29ccd32a9eda7ba11f1e0dbe1357c17ff
jshaw22 <jshaw22@gmail.com>
2016-03-01 18:53:38 -0800

    New API key for Face++

--- TinderTest.js
+++ TinderTest.js
@@ -2,33 +2,34 @@ var request = require('request');
 var tinder = require('tinderjs');
 var client = new tinder.TinderClient();
 
-var FB_USER_TOKEN = 'CAAGm0PX4ZCpsBAO8njUhAKIKuq30ZANBqTa3VEjdrgojm9C8c4VcPzkPktMoMvLqcD6y7KcubnNVnkuDsoiAPDwjYsvZBwgrV33SLRthfVzDZCPhsWaP5ChICRm9gsrcoJQhZBYNNmerLD4g0BmOSCIODCibruUrykaZBcT7SYM1m1KPBQaOtnDbXokcAdGcPzsM28hj32wWZClBqL5ZCM4z';
+var FB_USER_TOKEN = 'CAAGm0PX4ZCpsBAEHm0PkDfM45VbOktP6PioSCaajuQzGoSC9JRUs28tiFT4xTqIfFezZCdiM3B6ZAfA1rWsSzwRxIZASIOahhwJyHgZA6s8LsJrBQHfiEfA9N4ZCrx4RKOu2xGZCkdI8XZBkbNPcO2jCdZCrkQW1cZAnk0kZBu1Omet06qveyrjZChzPFzLr0lnz6cpbOAqqip0nZBoRuypOFWhFk';
 var FB_USER_ID = '2539134351764';
 
 
 //var URL = "http://i.imgur.com/REdTb8a.jpg";
 var URL;
+var pictureCount = 0;
+var pictureObj = {};
 
-var requestCallback = function(error, response, body){
+var requestCallback = function(error, response, body, cb){
   console.log("Request Callback Invoked")
   if(!error && response.statusCode == 200){
+
     var parsed = JSON.parse(body)
     var attributes = parsed.face[0].attribute;
-    console.log(attributes);
-  }
+    cb(attributes);
+  } 
 }
 
-var runFacialRecognition = function(URL){
+var runFacialRecognition = function(URL, cb){
   var options = {
     url: 'https://faceplusplus-faceplusplus.p.mashape.com/detection/detect?url='+URL,
     headers: {
-      'X-Mashape-Key': 'IJ3moiqJsymshUNl41Sq09a38nMjp1OIrd9jsnJw6JjvebyGo5',
+      'X-Mashape-Key': 'H7SUTDerX7mshZfgezL0oKl0E8hYp1WKpHhjsn42V6rHAGX5ms',
       Accept: 'application/json'
     }
   }
-
   request(options, requestCallback);
-
 }
 
 //runFacialRecognition(options) 
@@ -37,12 +38,16 @@ client.authorize(
   FB_USER_ID,
   function() {
     client.getRecommendations(1, function(error, data){
-      var pictureCount = 0;
-      var pictureObj = {};
+
       data.results[0].photos.forEach(function(photo){
         URL = photo.url;
         console.log(URL)
-        runFacialRecognition(URL);
+        runFacialRecognition(URL, function(val){
+          client.like(data._userID, function(val){
+            console.log("Liked!")
+          })
+        });
+
       });
     });
   });
